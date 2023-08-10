7a8c2e591eb94a3a245d4f00264b2e2749b07ddc
SantiRosso <santiagorosso@live.com.ar>
2023-08-10 18:22:03 -0300

    @Post(createTask)

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dtos/task.dto';
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
   @Get()
-  findAllTask() {
-    return this.taskService.getAllTask();
+  async findAllTask(): Promise<Task[]> {
+    const tasks: Task[] = await this.taskService.getAllTask();
+    return tasks;
+  }
+  @Post()
+  async createTask(@Body() task: CreateTaskDto): Promise<Task> {
+    const newTask: Task = await this.taskService.createTask(task);
+    return newTask;
   }
 }
--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid';
+import { CreateTaskDto } from 'src/dtos/task.dto';
 
 @Injectable()
 export class TasksService {
-  private task: Task[] = [
+  //esto simula una base de datos
+  private tasks: Task[] = [
     {
-      id: 1,
+      id: '1',
       title: 'first task',
       description: 'some task',
       status: TaskStatus.PENDING,
     },
     {
-      id: 2,
+      id: '2',
       title: 'second task',
       description: 'some task',
       status: TaskStatus.PENDING,
     },
     {
-      id: 3,
+      id: '3',
       title: 'third task',
       description: 'some task',
       status: TaskStatus.PENDING,
     },
   ];
-  getAllTask() {
-    return this.task;
+  async getAllTask(): Promise<Task[]> {
+    return this.tasks;
   }
-  createTask() {}
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const newTask: Task = {
+      id: v4(),
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push(newTask);
+    return newTask;
+  }
   updateTask() {}
   deleteTask() {}
 }
